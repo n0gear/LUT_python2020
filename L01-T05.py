e6d63f3cff3f8f9f0bd59869481e77c4520bbf7e
n0gear <kh@HILLA-7H38I5S.localdomain>
2020-09-08 21:33:54 +0300

    L01 tehtävät valmiit

---
+++ L01-T05.py
@@ -0,0 +1,8 @@
+tuote1 = input('Anna tuotteen 1 hinta: ')
+tuote2 = input('Anna tuotteen 2 hinta: ')
+hinta1 = int(tuote1)
+hinta2 = int(tuote2)
+summa = hinta1 + hinta2
+summa2 = str(summa)
+print('Tuote 1 maksaa ' + tuote1 + ' e ja tuote 2 maksaa ' + tuote2 + ' e.')
+print('Tuotteiden summa on ' + summa2 + ' euroa.')
---
+++ L01-T4.py
@@ -0,0 +1,2 @@
+nimi = input("Kerro nimesi: ")
+print('Tervetuloa koodari ' + nimi)
