d214fa577c9839b3d580828a9b46de81ba8aee24
n0gear <kh@HILLA-7H38I5S.localdomain>
2020-09-08 21:02:26 +0300

    L01-T02

---
+++ L01-T2.py
@@ -0,0 +1,8 @@
+luku1 = 50
+luku2 = 120
+
+
+print(luku1, luku2)
+print(luku1, "Tämä teksti tulee väliin", luku2)
+print("Uniikki unikorni olikin ainoastaan korni koni")
+# print(luku1, luku2, sep=" Tämä teksti tulee väliin ")
