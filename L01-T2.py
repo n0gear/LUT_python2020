b9bc5adf8826090d659be5570f100f945f955106
n0gear <kh@HILLA-7H38I5S.localdomain>
2020-09-08 21:15:23 +0300

    L01-T03

--- L01-T2.py
+++ L01-T2.py
@@ -1,7 +1,6 @@
 luku1 = 50
 luku2 = 120
 
-
 print(luku1, luku2)
 print(luku1, "Tämä teksti tulee väliin", luku2)
 print("Uniikki unikorni olikin ainoastaan korni koni")
---
+++ L01-T3.py
@@ -0,0 +1,13 @@
+luku = 5
+luku1 = luku + 34
+luku2 = luku1 * 3
+luku3 = luku2 - 6
+print(luku3)
+
+luku = 5
+luku = (luku + 34) * 3 - 6
+print(luku)
+
+luku1 = 13
+luku2 = 2
+print(luku1 % luku2)
