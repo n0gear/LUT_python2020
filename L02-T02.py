634301a2fa629de8c4b8e5a099ee59e34416a063
n0gear <kh@HILLA-7H38I5S.localdomain>
2020-09-10 15:29:06 +0300

    L02-T02

---
+++ L02-T02.py
@@ -0,0 +1,6 @@
+etunimi = input('Anna etunimi: ')
+sukunimi = input('Anna sukunimi: ')
+print('Nimeni on', sukunimi + ',', etunimi, sukunimi + '.')
+sana1 = input('Anna sana: ')
+sana2 = input('Anna toinen sana: ')
+print("Sanoista tulee yhdyssana","'" + sana1 + sana2 + "'" + '.')
