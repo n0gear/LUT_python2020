bf849ad2e7da17f8e4d754d8379aafd7e617e0e7
n0gear <kh@HILLA-7H38I5S.localdomain>
2020-09-10 16:13:41 +0300

    T03 parannukset

--- L02-T03.py
+++ L02-T03.py
@@ -1,3 +1,18 @@
 long = input('Anna pitkä sana: ')
-long[0:6:1]
-print(long)
+print('Antamasi sanan viisi ensimmäistä kirjainta ovat', long[0:5])
+print('Viisi viimeistä kirjainta ovat', long[-5:])
+print('Kirjaimet 2,3,4 ja 5 ovat', long[1:5])
+print()
+print('Sanan joka toinen kirjain alkaen toisesta kirjaimesta:', long[1::2])
+print()
+print('Annoit sanan', '\'' + long + '\',', 'joka on takaperin', '\'' + long[::-1] + '\'.')
+print()
+aloitus = int(input('Anna aloituspaikka: '))
+lopetus = int(input('Anna lopetuspaikka: '))
+siirtyma = int(input('Anna siirtymä: '))
+
+print('Antamillasi asetuksilla sana', long, 'tulostuu näin:', long[aloitus:lopetus:siirtyma])
+
+pituus = len(long)
+print()
+print('Sana oli', pituus, 'merkkiä pitkä.')
