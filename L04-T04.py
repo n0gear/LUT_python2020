4f8e7193bba2365bed4b9920e9e37ceb1eaff417
n0gear <kh@HILLA-7H38I5S.localdomain>
2020-09-22 22:09:03 +0300

    L04 tehtävät tehty onnistuneesti

--- L04-T04.py
+++ L04-T04.py
@@ -1,2 +1,11 @@
 alaraja = int(input('Anna alueen alaraja: '))
-ylaraja = int(input('Anna alueen ylaraja: '))
+ylaraja = int(input('Anna alueen yläraja: '))
+for i in range(alaraja, ylaraja + 1):
+    if (i % 5 != 0):
+        print(i, 'ei ole jaollinen viidellä, hylätään.')
+    elif (i % 7 != 0):
+        print(i, 'ei ole jaollinen seitsemällä, hylätään.')
+    else:
+        print('Luku', i, 'on jaollinen 5:llä ja 7:llä.')
+        break
+print('Lopetetaan etsintä.')
---
+++ L04-T05.py
@@ -0,0 +1,22 @@
+alaraja = int(input('Anna alueen alaraja: '))
+ylaraja = int(input('Anna alueen yläraja: '))
+alkuluku = 0
+luku = 0
+for numero in range(alaraja, ylaraja + 1):
+    if ((numero == 0) or (numero == 1)):
+        print(numero, 'ei ole kelvollinen alkuluku.')
+        luku = luku + 1
+    else:
+        for i in range(2, numero):
+            if ((numero % i) == 0):
+                print(numero, 'ei ole alkuluku, koska', i, '*', int(numero/i), '=', int(i * (numero/i)))
+                luku = luku + 1
+                break
+        else:
+            print(numero, 'on alkuluku.')
+            viimeinen_al = str(numero)
+            alkuluku = alkuluku + 1
+print('Tutkittiin', alkuluku + luku, 'lukua, joista', alkuluku, 'oli alkulukuja.')
+print('Viimeinen löydetty alkuluku oli', viimeinen_al + ('.'))
+print('Kiitos ohjelman käytöstä.')
+    
